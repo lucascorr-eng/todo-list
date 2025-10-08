ec9ad837181c84c91147e96ce1a4c4fa88205f12
Lucas Mint <lucas.engsoftwareee@gmail.com>
2025-10-08 11:44:51 -0300

    finished dialog modal

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ addTask.addEventListener("click", () => {
   } 
 })
 
+
+const closeDialog = document.querySelector(".close")
+
+closeDialog.addEventListener("click", () => {
+  if (box.style.display === "block") {
+    box.style.display = "none"
+  }
+})
+
+
+const form = document.getElementById("formAdd");
+const input = document.getElementById("descInput");
+const list = document.getElementById("toDoList");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();                 // <— impede comportamento padrão
+
+  const text = input.value.trim();
+  if (!text) return;
+
+  const li = document.createElement("li");
+  li.className = "list";
+  li.innerHTML = `
+    <p>${text}</p>
+    <button id="deleteList">
+      <img src="assets/trash.svg" alt="">
+    </button>
+  `;
+
+  list.appendChild(li);
+  form.reset();
+  box.style.display = "none";
+})
